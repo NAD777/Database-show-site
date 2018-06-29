d31a36f28f45c333fd792c62cf9b610857a2da9c
NAD777 <nehaev07.07.2003@gmail.com>
2018-06-29 23:45:51 +0300

    buttons for show form

--- app.py
+++ app.py
@@ -1,19 +1,37 @@
 from flask import Flask
 from flask import render_template
 from flask import request
+from flask import redirect
+
 from main import db
 
 app = Flask(__name__)
 app.debug=True
 
 @app.route('/')
-def hello_world():
+def index():
     database = db()
     results = database.get_list()
     database.close()
     return render_template("index.html", results=results)
-
 @app.route('/', methods=['POST'])
+def index1():
+    database = db()
+    results = database.get_list()
+    if(request.form['btn'] == 'Search'):
+        search = request.form['search']
+        results = database.get_list(search)
+    database.close()
+    return render_template("index.html", results=results)
+
+@app.route('/form')
+def my_form_out_with_table():
+    database = db()
+    results = database.get_list()
+    database.close()
+    return render_template('form.html',results = results)
+
+@app.route('/form', methods=['POST'])
 def my_form_post():
     database = db()
     ############################## cheacking for main form
@@ -37,11 +55,13 @@ def my_form_post():
         ########################## cheacking for main form
         database.commit(name,surname,patronymic,gender,inspection)
         results = database.get_list()
-        return render_template("index.html", results=results, result="all done")
+        # return render_template("index.html", results=results, result="all done")
+        return redirect('/')
     elif(request.form['btn']=='Search'):
         search = request.form['search']
         results = database.get_list(search)
-        return render_template("index.html", results=results)
+        # return render_template("index.html", results=results)
+        return render_template('form.html', results=results)
     else:
         print('nothing happened')
     database.close()
